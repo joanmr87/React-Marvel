0226a34e45f7086950dc175d8593d83ce36b95e6
joanMRomero <joanmr87@gmail.com>
2021-07-13 19:18:07 -0300

    algun avance

--- src/components/HeroeDetail.jsx
+++ src/components/HeroeDetail.jsx
@@ -1,7 +1,9 @@
 import React from 'react';
+import '../assets/styles/HeroeDetail.css'
 
 const HeroeDetail = (props) => (
-    <div className="HeroContainer">        
+    <div className="HeroContainer">   
+        <img className= "image-heroe" src={props.heroe.thumbnail.path + '.jpg'} alt='imagen heroe'/>     
         <h2>{props.heroe.name}</h2>
     </div>
 );
